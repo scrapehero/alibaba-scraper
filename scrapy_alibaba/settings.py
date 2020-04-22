7319dcf543dd8fc34cb8eb68fd53cf24443c8e7b
scrapehero-code <scrapehero@gmail.com>
2020-04-23 05:20:03 +0530

    Used selectorlib for Extraction & Added Pagination

--- scrapy_alibaba/settings.py
+++ scrapy_alibaba/settings.py
@@ -39,10 +39,20 @@ ROBOTSTXT_OBEY = False
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+    'authority': 'www.alibaba.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.alibaba.com/sw.js?v=2.13.12&_flasher_manifest_=https://s.alicdn.com/@g/flasher-manifest/icbu-v2/manifestB.json',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
--- scrapy_alibaba/spiders/alibaba_crawler.py
+++ scrapy_alibaba/spiders/alibaba_crawler.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import Request
 import csv
 import os
-
+from selectorlib import Extractor
+import re 
 
 class AlibabaCrawlerSpider(scrapy.Spider):
     name = 'alibaba_crawler'
     allowed_domains = ['alibaba.com']
     start_urls = ['http://alibaba.com/']
+    extractor = Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__), "../resources/search_results.yml"))
+    max_pages = 5
 
     def start_requests(self):
         """Read keywords from keywords file amd construct the search URL"""
@@ -22,61 +26,18 @@ class AlibabaCrawlerSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        """Function to process alibaba search results page"""
-        search_keyword=response.meta["search_text"]
-        products=response.xpath("//div[@class='item-main']")
-        # iterating over search results
-        for product in products:
-            # Defining the XPaths
-            XPATH_PRODUCT_NAME=".//div[@class='item-info']//h2[contains(@class,'title')]//a/@title"
-            XPATH_PRODUCT_PRICE=".//div[@class='item-info']//div[@class='price']/b/text()"
-            XPATH_PRODUCT_MIN_ORDER=".//div[@class='item-info']//div[@class='min-order']/b/text()"
-            XPATH_SELLER_YEARS=".//div[@class='item-info']//div[@class='stitle util-ellipsis']//div[contains(@class,'supplier-year')]//text()"
-            XPATH_SELLER_NAME=".//div[@class='item-info']//div[@class='stitle util-ellipsis']//a/@title"
-            XPATH_SELLER_RESPONSE_RATE=".//div[@class='item-info']//div[@class='sstitle']//div[@class='num']/i/text()"
-            XPATH_TRANSACTION_LEVEL=".//div[@class='item-info']//div[@class='sstitle']//a[@class='diamond-level-group']//i[contains(@class,'diamond-level-one')]"
-            XPATH_TRANSACTION_LEVEL_FRACTION=".//div[@class='item-info']//div[@class='sstitle']//a[@class='diamond-level-group']//i[contains(@class,'diamond-level-half-filled')]"
-            XPATH_PRODUCT_LINK=".//div[@class='item-info']//h2/a/@href"
-
-            raw_product_name=product.xpath(XPATH_PRODUCT_NAME).extract()
-            raw_product_price=product.xpath(XPATH_PRODUCT_PRICE).extract()
-            raw_minimum_order=product.xpath(XPATH_PRODUCT_MIN_ORDER).extract()
-            raw_seller_years=product.xpath(XPATH_SELLER_YEARS).extract()
-            raw_seller_name=product.xpath(XPATH_SELLER_NAME).extract()
-            raw_seller_response_rate=product.xpath(
-                XPATH_SELLER_RESPONSE_RATE).extract()
-            raw_transaction_level=product.xpath(
-                XPATH_TRANSACTION_LEVEL).extract()
-            raw_product_link=product.xpath(XPATH_PRODUCT_LINK).extract()
-            # getting the fraction part
-            raw_transaction_level_fraction=product.xpath(
-                XPATH_TRANSACTION_LEVEL_FRACTION)
-
-            # cleaning the data
-            product_name=''.join(raw_product_name).strip(
-            ) if raw_product_name else None
-            product_price=''.join(raw_product_price).strip(
-            ) if raw_product_price else None
-            minimum_order=''.join(raw_minimum_order).strip(
-            ) if raw_minimum_order else None
-            seller_years_on_alibaba=''.join(
-                raw_seller_years).strip() if raw_seller_years else None
-            seller_name=''.join(raw_seller_name).strip(
-            ) if raw_seller_name else None
-            seller_response_rate=''.join(raw_seller_response_rate).strip(
-            ) if raw_seller_response_rate else None
-            # getting actual transaction levels by adding the fraction part
-            transaction_level=len(raw_transaction_level)+.5 if raw_transaction_level_fraction else len(raw_transaction_level)
-            product_link="https:" + raw_product_link[0] if raw_product_link else None
-
-            yield {
-                'product_name': product_name,
-                'product_price': product_price,
-                'minimum_order': minimum_order,
-                'seller_years_on_alibaba': seller_years_on_alibaba,
-                'seller_name': seller_name,
-                'seller_response_rate': seller_response_rate,
-                'transaction_level': transaction_level,
-                'product_link': product_link,
-                'search_text': search_keyword
-            }
+        data = self.extractor.extract(response.text,base_url=response.url)
+        for product in data['products']:
+            yield product
+        
+        # Try paginating if there is data
+        if data['products']:
+            if '&page=' not in response.url and self.max_pages>=2:
+                yield Request(response.request.url+"&page=2")
+            else:
+                url = response.request.url
+                current_page_no = re.findall('page=(\d+)',url)[0]
+                next_page_no = int(current_page_no)+1
+                url = re.sub('(^.*?&page\=)(\d+)(.*$)',rf"\g<1>{next_page_no}\g<3>",url)
+                if next_page_no <= self.max_pages:
+                    yield Request(url,callback=self.parse)
